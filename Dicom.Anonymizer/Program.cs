954ad90dc8b54cbc049c96a9bcfde101b6bcb4c7
nikita tugolukov <tugolukov.obn@gmai.com>
2019-11-19 17:01:40 +0300

    Initial commit

---
+++ Dicom.Anonymizer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Dicom.Anonymizer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
